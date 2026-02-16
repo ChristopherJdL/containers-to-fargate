224e6dbe655c32b4c44a075e64051184190838c8
christopherjdl <christopher.jdl@outlook.com>
2026-02-16 09:40:19 +0000

    fix: ecs configuration

---
+++ infra/modules/fargate/main.tf
@@ -0,0 +1,39 @@
+resource "aws_ecs_cluster" "ctf_cluster" {
+  name = "ctf-cluster"
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+    user_data = templatefile("modules/fargate/templates/ctf.json.tpl", {
+        app_image      = var.app_image
+        app_port       = var.app_port
+        fargate_cpu    = var.fargate_cpu
+        fargate_memory = var.fargate_memory
+        aws_region     = var.aws_region
+    })
+    execution_role_name = "my-execution-role"
+    execution_role_arn  = "arn:aws:iam::303981612052:role/ecsTaskExecutionRole"
+
+}
+
+resource "aws_ecs_task_definition" "ctf_task" {
+    family                   = "ctf-app-task"
+    execution_role_arn       = local.execution_role_arn
+    requires_compatibilities = ["FARGATE"]
+    cpu                      = var.fargate_cpu
+    memory                   = var.fargate_memory
+    
+    container_definitions = local.user_data
+}
+
+resource "aws_ecs_service" "ctf_service" {
+  name            = "ctf"
+  cluster         = aws_ecs_cluster.ctf_cluster.id
+  task_definition = aws_ecs_task_definition.ctf_task.arn
+  desired_count   = 3
+#   iam_role        = aws_iam_role.foo.arn
+#   depends_on      = [aws_iam_role_policy.foo]
+
+
+}
---
+++ infra/modules/fargate/templates/ctf.json.tpl
@@ -0,0 +1,22 @@
+[
+  {
+    "name": "ctf",
+    "image": "${app_image}",
+    "cpu": ${fargate_cpu},
+    "memory": ${fargate_memory},
+    "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "/ecs/cb-app",
+          "awslogs-region": "${aws_region}",
+          "awslogs-stream-prefix": "ecs"
+        }
+    },
+    "portMappings": [
+      {
+        "containerPort": ${app_port},
+        "hostPort": ${app_port}
+      }
+    ]
+  }
+]
